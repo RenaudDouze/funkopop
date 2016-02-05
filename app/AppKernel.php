f745a16913fda928aa7a0751ca9fdf94af73a95e
RenaudLerognon <renaud.lerognon@gmail.com>
2016-02-05 21:47:30 +0100

    [++][UP][Index] Up image resizer bundle, use LiipImagine

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -43,6 +43,7 @@ class AppKernel extends Kernel
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
 
             new Gregwar\ImageBundle\GregwarImageBundle(),
+            new Liip\ImagineBundle\LiipImagineBundle(),
 
             new AppBundle\AppBundle(),
         ];
