10ede12e4d424b571f9eabc00b3beb85d4614f0a
RenaudLerognon <renaud.lerognon@gmail.com>
2016-04-06 22:54:59 +0200

    [++][UP][Admin] Liste des tags pour l'autocompletion directement chargé

--- web/js/admin.js
+++ web/js/admin.js
@@ -3,10 +3,11 @@
  *
  * @param  {string}       target
  * @param  {array|object} options
+ * @param  {array|object} autocompleteSource
  *
  * @return {object}       
  */
-function createTagsInput(target, options)
+function createTagsInput(target, options, autocompleteSource)
 {
     var tagsInputId = target.replace('tags-container-', '');
     var tagsInput = $('#' + tagsInputId);
@@ -47,35 +48,39 @@ function createTagsInput(target, options)
     var container = taggle.getContainer();
     var input = taggle.getInput();
 
+    autocompleteSource = (('undefined' === typeof autocompleteSource) ? 
+      function(request, response) {
+        $.ajax({
+          url: Routing.generate('app_admin_tagautocomplete'),
+          data: {
+            input: request.term
+          },
+          success: function(data) {
+            response(data);
+          }
+        });
+      } : autocompleteSource
+    );
+
     //*
     $(input).autocomplete({
         appendTo: container,
         position: { at: "left bottom", of: container },
-        source: function(request, response) {
-            $.ajax({
-              url: Routing.generate('app_admin_tagautocomplete'),
-              data: {
-                input: request.term
-              },
-              success: function(data) {
-                response(data);
-              }
-            });
-          },
-          minLength: 1,
-          select: function(event, data) {
-            event.preventDefault();
+        source: autocompleteSource,
+        minLength: 1,
+        select: function(event, data) {
+          event.preventDefault();
 
-            if (1 === event.which) {
-                taggle.add(data.item.value);
-            }
-          },
-          open: function() {
-            $(this).removeClass("ui-corner-all").addClass("ui-corner-top");
-          },
-          close: function() {
-            $(this).removeClass("ui-corner-top").addClass("ui-corner-all");
+          if (1 === event.which) {
+              taggle.add(data.item.value);
           }
+        },
+        open: function() {
+          $(this).removeClass("ui-corner-all").addClass("ui-corner-top");
+        },
+        close: function() {
+          $(this).removeClass("ui-corner-top").addClass("ui-corner-all");
+        }
     });
     /**/
 }
